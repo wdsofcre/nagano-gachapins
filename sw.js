5389589bae3a00c1bfa9d3926685ced5701d332b
wdsofcre <wd.sofcre87@gmail.com>
2026-08-04 00:56:37 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nagano-gachapins-v7';
+const CACHE='nagano-gachapins-v12-reference';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./nagano-map.svg','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
